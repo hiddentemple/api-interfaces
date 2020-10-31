f332c274e96e22cb8d9b8a8aa59d8d0425b14f99
Vinson <55514029+vinsonchin@users.noreply.github.com>
2020-10-31 12:31:34 -0600

    international numbers

--- src/contact/phone.dto.ts
+++ src/contact/phone.dto.ts
@@ -1,11 +1,9 @@
-import {IsDefined, IsEnum, IsNumberString, IsOptional, IsPhoneNumber, IsUUID, Length} from "class-validator";
+import {IsDefined, Matches, IsEnum, IsNumberString, IsOptional, IsPhoneNumber, IsUUID, Length} from "class-validator";
 import {CategorizedDTO, PhoneEmailCategory} from "../core/category";
 import {Type} from "class-transformer";
 
 export class PhoneDTO extends CategorizedDTO {
     @IsDefined()
-    @IsNumberString( { no_symbols: true })
-    @Length(10, 10, {message: 'Phone number must be exactly 10 digits'})
-    @IsPhoneNumber('US')
+    @Matches(/^\+\d{5,15}$/)
     phoneNumber: string
 }
